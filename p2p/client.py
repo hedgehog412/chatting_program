4f3a95f04bdbd7ea11b847692dbe5291c7adf9bd
hedgehog412 <hedgehog412@gmail.com>
2018-04-01 16:25:55 +0900

    changed client and server

--- p2p/client.py
+++ p2p/client.py
@@ -1,17 +1,18 @@
 import requests
 import sys
 import random
-
+from flask import Flask
+app=Flask(__name__)
 
 class Client(object):
     ENDPOINT = 'http://127.0.0.1:5000'
     HASH = ''
     USERNAME = ''
-
+    PORT=0
     def __init__(self):
         self.HASH = self.generate_hash()
         # self.USERNAME = self.get_username()  TODO 
-
+        self.PORT=int(sys.argv[1])
         requests.post('{}/register-seed'.format(self.ENDPOINT), data={'hash':self.HASH})
 
 
@@ -20,3 +21,13 @@ class Client(object):
         for i in range(20):
             _hash += str(int(random.random() * 10))
         return _hash
+
+    def exit(self):
+        requests.post('{}/remove-seed'.format(self.ENDPOINT),data={'hash':self.HASH})
+
+    def join(self):
+        seed_hash=requests.post('{}/join-net'.format(self.ENDPOINT),data={'hash':self.HASH,})
+
+if __name__=='__main__':
+    c=Client()
+    app.run(host='127.0.0.1',port=c.PORT)
--- p2p/server.py
+++ p2p/server.py
@@ -2,22 +2,29 @@
 #
 ## KEEPS TRACK OF ALL THE SEED VALUES
 
-from flask import Flask
+from flask import Flask,request
 app = Flask(__name__)
 
 
 # List of all known seeds
 SEEDS = []
-
-
-@app.route('/register-seed')
+@app.route('/register-seed',methods=['GET','POST'])
 def register_seed():
     seed_value = request.values['hash']
+    seed_ip=request.remote_addr
+    print(seed_ip)
+    seed_port=request.environ.get('REMOTE_PORT')
+    SEEDS.append(seed_value)
     return 'ok'
 
 
 @app.route('/remove-seed')
 def remove_seed():
-    """ TODO """
+    SEEDS.remove(request.values['hash'])
 
-
+@app.route('/join-net')
+def register_peer():
+    idx=int(random.random()*len(SEEDS))
+    
+    return SEEDS[idx]
+app.run()
